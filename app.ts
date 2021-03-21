62005737368e592b542a4651dbcf5f55d4276981
cristianat98 <cristianarmestotejedo@gmail.com>
2021-03-21 13:29:24 +0100

    Server acabado

--- app.ts
+++ app.ts
@@ -1,17 +1,19 @@
 import express from 'express'
 import cors from 'cors';
+import { Request, Response} from "express";
 
 const port = 3000;
 
 const app = express()
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.post('/mensaje', (req, res) => {
-    res.json("RECIBIDO!!")
+app.post('/mensaje', (req: Request, res: Response) => {
+    res.json(req.body)
 })
 
 app.listen(port, () => {

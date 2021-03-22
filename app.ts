869addebe3fe73650071ed64892f53a02726e7ac
cristianat98 <cristianarmestotejedo@gmail.com>
2021-03-22 20:54:07 +0100

    Server RSA

--- app.ts
+++ app.ts
@@ -1,21 +1,42 @@
 import express from 'express'
-import cors from 'cors';
-import { Request, Response} from "express";
+import cors from 'cors'
+import * as bigintConversion from 'bigint-conversion'
+import * as rsa from './rsa'
 
-const port = 3000;
+const port = 3000
 
 const app = express()
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3001' // angular.js server
+}))
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+  res.send('hello world')
 })
 
-app.post('/mensaje', (req: Request, res: Response) => {
-    res.json(req.body)
+app.post('/mensaje', (req, res) => {
+  res.json(req.body);
 })
 
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
+app.get('/rsa', async function (req, res) {
+  const rsaKeys = await rsa.generateKeys(2048)
+  console.log(rsaKeys)
+  res.json({
+    publicKey: {
+      e: bigintConversion.bigintToHex(rsaKeys.publicKey.e),
+      n: bigintConversion.bigintToHex(rsaKeys.publicKey.n)
+    } 
+  })
+})
+
+app.get('/user', (req, res) => {
+  const user = {
+    username: 'walrus',
+    description: 'it is what it is'
+  }
+  res.json(user)
+})
+
+app.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`)
 })
